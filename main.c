ce1ae19232c30203af567a4ea2124d5c920568ae
Patiharn-Kamenk <122970698+Patiharn-Kamenkit@users.noreply.github.com>
2023-02-03 10:40:32 +0000

    finishhhhhhh

--- main.c
+++ main.c
@@ -1,36 +1,34 @@
+#include "sorting.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "sorting.h"
 
 int main(int argc, char *argv[]) {
   int i, *a, n;
 
   n = argc - 2;
-  a = (int *)malloc(sizeof(int) *n);
+  a = (int *)malloc(sizeof(int) * n);
   for (i = 0; i < n; i++) {
     a[i] = atoi(argv[i + 2]);
   }
 
-  printf("%s",argv[1]);
+  printf("%s", argv[1]);
 
-  if(strcmp(argv[1],"bubble")==0)
-  {
-    bubbleSort(a,n);
-    display(a,n);
+  if (strcmp(argv[1], "bubble") == 0) {
+    bubbleSort(a, n);
+    display(a, n);
+    printf("\n");
   }
-  if(strcmp(argv[1],"insertion")==0)
-  {
-    insertion(a,n);
-    display(a,n);
+  if (strcmp(argv[1], "insertion") == 0) {
+    insertion(a, n);
+    display(a, n);
+    printf("\n");
   }
-  if(strcmp(argv[1],"selection")==0)
-  {
-    selectionSort(a,n);
-    display(a,n);
+  if (strcmp(argv[1], "selection") == 0) {
+    selectionSort(a, n);
+    display(a, n);
+    printf("\n");
   }
- 
- return 0;
+  printf("\n");
+  return 0;
 }
-
-
