0e5e44074147ba5be1d4b048dbd0dbc086797ace
Patiharn-Kamenk <122970698+Patiharn-Kamenkit@users.noreply.github.com>
2023-02-03 10:37:22 +0000

    finishh

--- .ccls-cache/@home@runner@group-sorting-ppg/sorting.h
+++ .ccls-cache/@home@runner@group-sorting-ppg/sorting.h
@@ -8,70 +8,62 @@ void swap(int *a, int *b) {
   *b = temp;
 }
 
-void display(int a[],int n){
+void display(int a[], int n) {
 
-    int i;
+  int i;
 
-    for(i=0;i<n;i++)
-        printf("%5d",a[i]);
-    printf("\n");
-  
+  for (i = 0; i < n; i++)
+    printf("%5d", a[i]);
+  printf("\n");
 }
 
-void selectionSort(int a[], int n)
-{
-  int i,j;
+void selectionSort(int a[], int n) {
+  int i, j;
   int mi;
   int m;
 
-  for(j=0; j<n-1; j++)
-    {
-        m=a[j]; 
-        mi=j;   
-        for(i= j+1 ;i<n; i++){
-        if(m<a[i]) 
-        {
-          m=a[i];  
-          mi=i;
-        }
-  }
-  swap(&a[j],&a[mi]);
-  
-  printf("\n");
+  for (j = 0; j < n - 1; j++) {
+    m = a[j];
+    mi = j;
+    for (i = j + 1; i < n; i++) {
+      if (m < a[i]) {
+        m = a[i];
+        mi = i;
+      }
+    }
+    swap(&a[j], &a[mi]);
 
-  display(a,n);
-  }      
+    printf("\n");
+
+    display(a, n);
+  }
 }
 
-
-
-void insertion(int x[], int n) 
-{
-  int i,j;
+void insertion(int x[], int n) {
+  int i, j;
   int value = x[j];
 
-  for(j=1;j<n;j++)
-    {
-      int value = x[j];
-      for(i=j-1;i>=0;i--)
-      {
-        if(value<x[i]) break;
-        else x[i+1]=x[i];
-        display(x,n);
-      }
-        x[i+1]=value;
-      display(x,n);
-      printf("\n");
+  for (j = 1; j < n; j++) {
+    int value = x[j];
+    for (i = j - 1; i >= 0; i--) {
+      if (value < x[i])
+        break;
+      else
+        x[i + 1] = x[i];
+      display(x, n);
     }
-  
+    x[i + 1] = value;
+    display(x, n);
+    printf("\n");
+  }
 }
 
 void bubbleSort(int a[], int n) {
 
   int i, j;
   int sorted;
-  display(a,n);
-  for (i = 0; i < n ; i++) {
+  display(a, n);
+  for (i = 0; i < n; i++) {
     sorted = 0;
     for (j = 0; j < n - 1 - i; j++) {
       if (a[j] < a[j + 1]) {
@@ -84,12 +76,6 @@ void bubbleSort(int a[], int n) {
     if (sorted == 0)
       break;
   }
-  display(a,n);
+  display(a, n);
   printf("\n");
 }
-
-
-
-
-
-
